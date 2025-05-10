ebc0afd18eca34ce8e1e6abc0cb493fd9c71ac3e
Sade Phillips <contact@sadephillips.com>
2025-05-10 16:37:45 +0100

    Initial commit: basic project structure and setup.

---
+++ index.js
@@ -0,0 +1,14 @@
+const generateRandomNumber = (num)  => {
+    return Math.floor(Math.random() * num);
+}
+
+const createHoroscope = () => {
+    const signs = ['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn', 'Aqarius', 'Pisces' ];
+    const fortunes = ['You will experience significant career breakthroughs this month.', 'A long lost friend will suddenly walk back into your life', 
+        'Your aura is looking extra sparkly today—don\'t let boring people dim your shine', 'You\'re not lost—just taking the scenic route. Trust your detours today',
+        'Romance is in the air. So is pollen. Take antihistamines and stay open to love.', 'An accidental text will lead to a surprising opportunity. Type wisely.', 
+        'An unexpected but welcome surprise will materialize this month.', 'Your radiance will attract surprising fans', 'Someone you won\'t expect will be your cheerleader', 
+        'That question you thought about will be answered this month', 'A new door will open for you.' ];
+
+    const wordsOfWisdom = ['Prepare accordingly.', 'Trust your instincts', 'Don\'t rush patience will pay off', ]
+}
